152b76367e4cb743fd60095a24c6af736a40f810
Esha Pai <esha.pai254@gmail.com>
2026-01-06 16:18:21 -0500

    Initial Commit

---
+++ GenomeLens/GenomeLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenomeLensApp.swift
+//  GenomeLens
+//
+//  Created by Esha Pai on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct GenomeLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
